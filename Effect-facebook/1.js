25c9aea6718e8391d650128ab1910db286450687
nguyenan99 <andepzai1999@gmail.com>
2019-07-12 03:51:55 +0700

    first commit

---
+++ Effect-facebook/1.js
@@ -0,0 +1,48 @@
+﻿ document.addEventListener("DOMContentLoaded",function(){
+     var nut = document.getElementsByClassName('deClick');
+     var danhsach = document.getElementsByClassName('dehienthi');
+    //  console.log(danhsach);
+     
+    
+    for(var i=0;i<nut.length;i++)
+    {
+        nut[i].onclick = function(){
+            if(this.classList[1]=="mautrang")
+            {
+                this.classList.remove("mautrang");
+                var nd = this.getAttribute('data-thamso');
+                phantuhienra = document.getElementById(nd);
+                phantuhienra.classList.remove('ra');
+                
+            }
+            else{
+                for(k=0;k<nut.length;k++)
+                {
+                    if(k!=i)
+                    {
+                        nut[k].classList.remove('mautrang');
+                    }
+                   
+                }
+                this.classList.toggle('mautrang');
+                
+                var nd = this.getAttribute('data-thamso');
+                phantuhienra = document.getElementById(nd);
+                for (let j = 0; j < danhsach.length; j++) {
+                        danhsach[j].classList.remove('ra');
+                    
+                }
+                phantuhienra.classList.toggle('ra');
+
+            }
+            
+           
+            
+        }
+        
+
+    }
+    
+     
+ },false)
+ 
