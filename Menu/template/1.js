3cd443a02e6b33a2fb1b93c06225d16b84119275
nguyenan99 <andepzai1999@gmail.com>
2019-07-12 06:13:44 +0700

    first commit

---
+++ Menu/template/1.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+    var nut = document.getElementsByClassName('nut');
+    var menu = document.getElementsByClassName('menu');
+    var xam = document.getElementsByClassName('xam');
+    var khoito = document.getElementsByClassName('khoito');
+    
+        nut[0].onclick = function(){
+            //  menu[0].classList.toggle('ra');
+            xam[0].classList.toggle('hien');
+            khoito[0].classList.toggle('ra1');
+        }
+        xam[0].onclick = function(){
+            //  menu[0].classList.remove('ra');
+            xam[0].classList.remove('hien');
+            khoito[0].classList.remove('ra1');
+         }
+    
+},false)
+ 
